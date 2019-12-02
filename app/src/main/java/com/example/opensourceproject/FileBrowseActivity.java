65e8a4141d688bdbb56bceeb7dc5d9e93ab661f0
ggj0418 <ggj0418@naver.com>
2019-12-02 14:36:53 +0900

    add browse activity's content

--- app/src/main/java/com/example/opensourceproject/FileBrowseActivity.java
+++ app/src/main/java/com/example/opensourceproject/FileBrowseActivity.java
@@ -23,6 +23,15 @@ import java.util.ArrayList;
 public class FileBrowseActivity extends AppCompatActivity {
 
     private String userID, remarks;
+    ArrayList<UploadFile> uploadFileArrayList = new ArrayList<UploadFile>();
+
+    private APIInterface apiInterface;
+    private ListView listView;
+    private UploadFileAdapter uploadFileAdapter;
+
+    private TextView userIdText, fileContentText, policyAndText, policyOrText;
+    private TextView plainText, toUploadText;
+    private Button checkPolicyButton, DecryptButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +43,14 @@ public class FileBrowseActivity extends AppCompatActivity {
         Intent fromMainIntent = getIntent();
         userID = fromMainIntent.getExtras().getString("loginID");
         remarks = fromMainIntent.getExtras().getString("remarks");
+
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+
+        userIdText = (TextView) findViewById(R.id.browse_user_id);
+        fileContentText = (TextView) findViewById(R.id.browse_file_content);
+        policyAndText = (TextView) findViewById(R.id.browse_policy_and);
+        policyOrText = (TextView) findViewById(R.id.browse_policy_or);
+        plainText = (TextView) findViewById(R.id.plainText);
+        toUploadText = (TextView) findViewById(R.id.to_upload_text);
     }
 }
